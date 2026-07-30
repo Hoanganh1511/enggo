b036f1848e136ecda3c5966a1369eca5641b5183
Hoang Tuan Anh <anhht.fe@gmail.com>
2026-07-30 17:44:39 +0700

    update

--- src/components/career-tree/main-content-area.tsx
+++ src/components/career-tree/main-content-area.tsx
@@ -2,9 +2,7 @@
 // (xem (main)/layout.tsx). Khong con Sidebar nen khong phai dong bo
 // margin-left voi thu gi ca, chi la 1 div padding tinh + vung cuon.
 const MainContentArea = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className=" min-h-0 flex-1 overflow-auto pt-4 pb-4">{children}</div>
-  );
+  return <div className=" min-h-0 flex-1 overflow-auto pb-4">{children}</div>;
 };
 
 export default MainContentArea;
